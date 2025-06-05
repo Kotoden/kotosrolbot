613d1573f93ad6cedd1d68399ee05e3eb28adea5
Kotoden <andrei635312@gmail.com>
2025-06-06 00:49:31 +0200

    final

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 from os import getenv
 
-TOKEN: str = getenv("TELEGRAM_BOT_TOKEN", "6993240073:AAFHQ4Rk2RVXwjEakoxY5hCf1qrgIrIA-Ts")
+TOKEN: str = getenv("TELEGRAM_BOT_TOKEN", "TELEGRAM_BOT_TOKEN")
 DATABASE_URL: str = getenv("DATABASE_URL", "sqlite:///online_shop.db")
